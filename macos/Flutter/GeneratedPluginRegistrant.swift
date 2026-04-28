6804c93d99a91ee62aa6e36ffbdb6d7554c12511
Ian Portuguez Rodriguez <portuguezrodriguezian@gmail.com>
2026-04-28 16:14:08 -0600

    Agregaciones de funciones de audio, imagen y qr

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,18 @@
 import FlutterMacOS
 import Foundation
 
+import audioplayers_darwin
+import file_selector_macos
 import geolocator_apple
+import mobile_scanner
+import package_info_plus
+import record_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  RecordMacOsPlugin.register(with: registry.registrar(forPlugin: "RecordMacOsPlugin"))
 }
